fed4aea2e84f677b0ab9014f8651081d901705bf
Niklas Lundberg <info@nlundberg.se>
2016-01-06 20:23:12 +0100

    Added Arbor.X build tool.

---
+++ build/build.bat
@@ -0,0 +1,59 @@
+@ECHO OFF
+SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
+SET Arbor.X.Tools.External.MSpec.Enabled=true
+SET Arbor.X.NuGet.Package.Artifacts.Suffix=
+SET Arbor.X.NuGet.Package.Artifacts.BuildNumber.Enabled=
+SET Arbor.X.Log.Level=Debug
+SET Arbor.X.NuGetPackageVersion=
+SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled=false
+SET Arbor.X.Build.VariableOverrideEnabled=true
+SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=true
+SET Arbor.X.Build.NetAssembly.Configuration=
+
+SET Version.Major=0
+SET Version.Minor=1
+SET Version.Patch=0
+SET Version.Build=100
+
+IF "%Arbor.X.Vcs.Branch.Name%" == "" (
+	SET Arbor.X.Vcs.Branch.Name=develop
+)
+
+SET Arbor.X.NuGet.ReinstallArborPackageEnabled=true
+SET Arbor.X.NuGet.VersionUpdateEnabled=false
+SET Arbor.X.Artifacts.PdbArtifacts.Enabled=true
+SET Arbor.X.NuGet.Package.CreateNuGetWebPackages.Enabled=true
+
+SET Arbor.X.Build.NetAssembly.MetadataEnabled=true
+SET Arbor.X.Build.NetAssembly.Description=Key value configuration
+SET Arbor.X.Build.NetAssembly.Company=Niklas Lundberg
+SET Arbor.X.Build.NetAssembly.Copyright=© Niklas Lundberg 2014-2016
+SET Arbor.X.Build.NetAssembly.Trademark=Arbor.KVConfiguration
+SET Arbor.X.Build.NetAssembly.Product=Arbor.KVConfiguration
+SET Arbor.X.ShowAvailableVariablesEnabled=false
+SET Arbor.X.ShowDefinedVariablesEnabled=false
+SET Arbor.X.Tools.External.MSBuild.Verbosity=minimal
+SET Arbor.X.NuGet.Package.AllowManifestReWriteEnabled=false
+
+SET Arbor.X.Tools.External.MSBuild.CodeAnalysis.Enabled=true
+
+CALL "%~dp0\Build.exe"
+
+REM Restore variables to default
+
+SET Arbor.X.Build.Bootstrapper.AllowPrerelease=
+REM SET Arbor.X.Vcs.Branch.Name=
+SET Arbor.X.Tools.External.MSpec.Enabled=
+SET Arbor.X.NuGet.Package.Artifacts.Suffix=
+SET Arbor.X.NuGet.Package.Artifacts.BuildNumber.Enabled=
+SET Arbor.X.Log.Level=
+SET Arbor.X.NuGetPackageVersion=
+SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled
+SET Arbor.X.VariableOverrideEnabled=
+SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=
+SET Arbor.X.Build.NetAssembly.Configuration=
+
+SET Version.Major=
+SET Version.Minor=
+SET Version.Patch=
+SET Version.Build=
