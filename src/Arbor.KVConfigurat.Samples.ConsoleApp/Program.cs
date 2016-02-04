b12089202cea9ff7725da4972b391ffbc664405c
Niklas Lundberg <info@nlundberg.se>
2016-02-04 14:16:11 +0100

    Bumped version from 0.2.0 to 0.3.0.

---
+++ src/Arbor.KVConfigurat.Samples.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arbor.KVConfigurat.Samples.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new AppScenario1().Execute();
+
+            Console.WriteLine(new string('*', 50));
+
+            new AppScenario2().Execute();
+
+            Console.WriteLine(new string('*', 50));
+
+            new AppScenario3().Execute();
+        }
+    }
+}
