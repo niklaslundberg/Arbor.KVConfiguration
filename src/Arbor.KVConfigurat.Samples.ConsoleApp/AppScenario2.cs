60cfaacdbf3c426d5a1a6ef9de5c733359066244
Niklas Lundberg <info@nlundberg.se>
2016-02-04 14:15:37 +0100

    Added new console application sample.

---
+++ src/Arbor.KVConfigurat.Samples.ConsoleApp/AppScenario2.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+using Arbor.KVConfiguration.Core;
+
+namespace Arbor.KVConfigurat.Samples.ConsoleApp
+{
+    public class AppScenario2
+    {
+        public void Execute()
+        {
+            NameValueCollection collection = new NameValueCollection
+                                                 {
+                                                     { string.Empty, string.Empty },
+                                                     { null, null },
+                                                     { null, string.Empty },
+                                                     { string.Empty, null },
+                                                     { "\t", "\t" },
+                                                     { "urn:test:key", "a-test-value" },
+                                                     { "urn:test:KEY", "second-test-value" },
+                                                     { "urn:another-key", "another-test-value" },
+                                                 };
+
+            IKeyValueConfiguration appSettingsKeyValueConfiguration = new InMemoryKeyValueConfiguration(collection);
+
+            KVConfigurationManager.Initialize(appSettingsKeyValueConfiguration);
+
+            var goodKeys = new List<string>()
+                               {
+                                   "a-non-existing-key",
+                                   "urn:test:key",
+                                   "urn:TEST:key",
+                                   "urn:test:KEY",
+                                   "urn:another-key"
+                               };
+
+            var keys = Specials.Special.ToList();
+            keys.AddRange(goodKeys.Select(goodKey => new KeyValuePair<string, string>(goodKey, goodKey)));
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var pair in keys)
+            {
+                builder.AppendLine($"Key: {pair.Key}");
+
+                string value = appSettingsKeyValueConfiguration.ValueOrDefault(pair.Value);
+
+                var displayValue = Specials.GetDisplayValue(value);
+
+                builder.AppendLine($"\t Instance: {displayValue}");
+
+                var staticValue = KVConfigurationManager.AppSettings[pair.Value];
+
+                var staticDisplayValue = Specials.GetDisplayValue(staticValue);
+
+                builder.AppendLine($"\t Static: {staticDisplayValue}");
+            }
+
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
---
+++ src/Arbor.KVConfigurat.Samples.ConsoleApp/Specials.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arbor.KVConfigurat.Samples.ConsoleApp
+{
+    public static class Specials
+    {
+        public static readonly Dictionary<string, string> Special = new Dictionary<string, string>
+                                                                        {
+                                                                            {
+                                                                                "[EMPTY]", string.Empty
+                                                                            },
+                                                                            {
+                                                                                "[NULL]", null
+                                                                            },
+                                                                            {
+                                                                                "[BLANK]", "\t"
+                                                                            }
+                                                                        };
+
+        public static string GetDisplayValue(string value)
+        {
+            var keyValuePairs = Special
+                .Where(pair => Equals(value, pair.Value))
+                .ToArray();
+
+            if (!keyValuePairs.Any())
+            {
+                return value;
+            }
+
+            var first = keyValuePairs.First();
+
+            return first.Key;
+        }
+    }
+}
