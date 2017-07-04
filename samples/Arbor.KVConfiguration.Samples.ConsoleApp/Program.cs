69d4fcf5ff8ad3fe1c2c2c8e9a6366d48844908a
Niklas Lundberg <info@nlundberg.se>
2017-07-04 15:19:04 +0200

    Updated more projects to .NET core.
    Fixed nuspec versions.

---
+++ samples/Arbor.KVConfiguration.Samples.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arbor.KVConfiguration.Samples.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/Arbor.KVConfiguration.Tests.Unit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arbor.KVConfiguration.Tests.Unit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
