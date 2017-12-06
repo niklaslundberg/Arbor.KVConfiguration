0963896478b603a4fd851286f27b90e4a44c4cf3
Niklas Lundberg <info@nlundberg.se>
2017-12-06 12:34:45 +0100

    Re-implemented configuration providers on top of Microsoft.Configuration.ConfigurationBuilders.Base.

--- src/Arbor.KVConfiguration.SystemConfiguration.Providers.Json/JsonKeyValueConfigurationBuilder.cs
+++ src/Arbor.KVConfiguration.SystemConfiguration.Providers.Json/JsonKeyValueConfigurationBuilder.cs
@@ -1,14 +1,55 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.IO;
 using Arbor.KVConfiguration.Core;
+using Microsoft.Configuration.ConfigurationBuilders;
 
 namespace Arbor.KVConfiguration.SystemConfiguration.Providers.Json
 {
     public class JsonKeyValueConfigurationBuilder : KeyValueConfigurationBuilder
     {
+        private const string JsonFilePropertyName = "jsonFile";
+        private const string IgnoreMissingFilePropertyName = "ignoreMissingFile";
+
+        private bool _ignoreMissingFile;
+        private string _jsonFilePath;
+
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException($"{nameof(JsonKeyValueConfigurationBuilder)} is missing value for its 'name' property", nameof(name));
+            }
+
+            _ignoreMissingFile = !bool.TryParse(config[IgnoreMissingFilePropertyName], out bool result) || result;
+
+            string jsonFile = config[JsonFilePropertyName];
+
+            if (string.IsNullOrWhiteSpace(jsonFile))
+            {
+                throw new ArgumentException(
+                    $"{nameof(JsonKeyValueConfigurationBuilder)} '{name}': Json file must be specified with the '{JsonFilePropertyName}' attribute.");
+            }
+
+            _jsonFilePath = Utils.MapPath(jsonFile);
+
+            if (!_ignoreMissingFile && !File.Exists(_jsonFilePath))
+            {
+                throw new ArgumentException(
+                    $"{nameof(JsonKeyValueConfigurationBuilder)} '{name}': Json file does not exist.");
+            }
+
+            base.Initialize(name, config);
+        }
+
         protected override IKeyValueConfiguration GetKeyValueConfiguration()
         {
-            var jsonConfiguration = new JsonConfiguration.JsonKeyValueConfiguration(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "settings.json"), throwWhenNotExists: false);
+            var jsonConfiguration = new JsonConfiguration.JsonKeyValueConfiguration(_jsonFilePath, throwWhenNotExists: !_ignoreMissingFile);
 
             return jsonConfiguration;
         }
