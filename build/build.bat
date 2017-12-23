cba1ab11fa79f3145316b1d047391324f5039d95
Niklas Lundberg <info@nlundberg.se>
2017-12-23 23:56:47 +0100

    Removed default branch name in build script.

--- build/build.bat
+++ build/build.bat
@@ -10,11 +10,6 @@ SET Arbor.X.Build.VariableOverrideEnabled=true
 SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=true
 SET Arbor.X.Build.NetAssembly.Configuration=
 SET Arbor.X.Tools.External.MSBuild.AllowPrerelease.Enabled=true
-
-IF "%Arbor.X.Vcs.Branch.Name%" == "" (
-	SET Arbor.X.Vcs.Branch.Name=develop
-)
-
 SET Arbor.X.Build.WebProjectsBuild.Enabled=false
 SET Arbor.X.MSBuild.NuGetRestore.Enabled=true
 SET Arbor.X.NuGet.ReinstallArborPackageEnabled=true
