2d7ca1867118e5447e4a4791c8aac706570c2263
Niklas Lundberg <info@nlundberg.se>
2018-09-30 22:14:17 +0200

    Fixed user config parent folder lookup.

---
+++ test/Arbor.KVConfiguration.Tests.Unit/when_getting_user_settings_when_it_does_not_exist.cs
@@ -0,0 +1,18 @@
+﻿using Arbor.KVConfiguration.Core;
+using Machine.Specifications;
+
+namespace Arbor.KVConfiguration.Tests.Unit
+{
+    [Subject(typeof(UserConfiguration.UserConfiguration))]
+    public class when_getting_user_settings_when_it_does_not_exist
+    {
+        private static MultiSourceKeyValueConfiguration configuration;
+
+        private Because of = () =>
+            configuration = KeyValueConfigurationManager.Add(new UserConfiguration.UserConfiguration()).Build();
+
+        private It should_not_be_part_of_source_chain = () =>
+            configuration.SourceChain.ShouldEqual(
+                "source chain: Arbor.KVConfiguration.UserConfiguration.UserConfiguration [no json file source]");
+    }
+}
