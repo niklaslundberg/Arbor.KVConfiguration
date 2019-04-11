04edacb90957a99716d36a8ed4cd5fe66ef686e5
Niklas Lundberg <info@nlundberg.se>
2019-04-11 21:35:46 +0200

    Added app settings builder extensions.

---
+++ src/Arbor.KVConfiguration.Core/Extensions/EnvironmentVariables/EnvironmentVariablesAppExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Arbor.KVConfiguration.Core.Extensions.EnvironmentVariables
+{
+    public static class EnvironmentVariablesAppExtensions
+    {
+        public static AppSettingsBuilder AddEnvironmentVariables(
+            this AppSettingsBuilder builder,
+            IReadOnlyDictionary<string, string> environmentVariables)
+        {
+            if (environmentVariables is null)
+            {
+                return builder;
+            }
+
+            var nameValueCollection = new NameValueCollection();
+
+            foreach (KeyValuePair<string, string> environmentVariable in environmentVariables)
+            {
+                nameValueCollection.Add(environmentVariable.Key, environmentVariable.Value);
+            }
+
+            return builder.Add(new InMemoryKeyValueConfiguration(nameValueCollection));
+        }
+    }
+}
---
+++ test/Arbor.KVConfiguration.Tests.Unit/CommandLine/CommandLineArgsTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Arbor.KVConfiguration.Core;
+using Arbor.KVConfiguration.Core.Extensions.CommandLine;
+using Xunit;
+
+namespace Arbor.KVConfiguration.Tests.Unit.CommandLine
+{
+    public class CommandLineArgsTests
+    {
+        [Fact]
+        public void WhenParsingMultipleSimpleValueWithSameKeyItShouldBeResolvableInConfiguration()
+        {
+            string[] args = { "abc=123", "abc=123" };
+
+            IKeyValueConfiguration keyValueConfiguration = args.ToKeyValueConfiguration();
+
+            string[] actualStrings = keyValueConfiguration.AllWithMultipleValues[0].Values.ToArray();
+            Assert.Equal(new[] { "123", "123" }, actualStrings);
+        }
+
+        [Fact]
+        public void WhenParsingSimpleValueItShouldBeResolvableInConfiguration()
+        {
+            string[] args = { "abc=123" };
+
+            IKeyValueConfiguration keyValueConfiguration = args.ToKeyValueConfiguration();
+
+            Assert.Equal("123", keyValueConfiguration["abc"]);
+        }
+    }
+}
