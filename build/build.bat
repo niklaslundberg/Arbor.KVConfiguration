414977fd50715dcf344848fa74b78410b5d3bb53
Niklas Lundberg <info@nlundberg.se>
2019-12-29 00:42:58 +0100

    Fixed variable names.

--- build/build.bat
+++ build/build.bat
@@ -1,53 +1,53 @@
 @ECHO OFF
-SET Arbor.X.Log.Level=Verbose
-SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
-SET Arbor.X.Tools.External.MSpec.Enabled=true
-SET Arbor.X.NuGet.Package.Artifacts.Suffix=
-SET Arbor.X.NuGet.Package.Artifacts.BuildNumber.Enabled=
-SET Arbor.X.NuGetPackageVersion=
-SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled=true
-SET Arbor.X.Vcs.Branch.Name=%GITHUB_REF%
-SET Arbor.X.Build.VariableOverrideEnabled=true
-SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=true
-SET Arbor.X.Build.NetAssembly.Configuration=
-SET Arbor.X.Tools.External.MSBuild.AllowPrerelease.Enabled=false
-SET Arbor.X.Build.WebProjectsBuild.Enabled=false
-SET Arbor.X.MSBuild.NuGetRestore.Enabled=true
-SET Arbor.X.NuGet.ReinstallArborPackageEnabled=true
-SET Arbor.X.NuGet.VersionUpdateEnabled=false
-SET Arbor.X.Artifacts.PdbArtifacts.Enabled=true
-SET Arbor.X.NuGet.Package.CreateNuGetWebPackages.Enabled=false
-SET Arbor.X.Tools.External.Xunit.NetCoreApp.Enabled=false
-SET Arbor.X.Tools.External.Xunit.NetFramework.Enabled=false
+SET Arbor.Build.Log.Level=Verbose
+SET Arbor.Build.Build.Bootstrapper.AllowPrerelease=true
+SET Arbor.Build.Tools.External.MSpec.Enabled=true
+SET Arbor.Build.NuGet.Package.Artifacts.Suffix=
+SET Arbor.Build.NuGet.Package.Artifacts.BuildNumber.Enabled=
+SET Arbor.Build.NuGetPackageVersion=
+SET Arbor.Build.Vcs.Branch.Name.Version.OverrideEnabled=true
+SET Arbor.Build.Vcs.Branch.Name=%GITHUB_REF%
+SET Arbor.Build.Build.VariableOverrideEnabled=true
+SET Arbor.Build.Artifacts.CleanupBeforeBuildEnabled=true
+SET Arbor.Build.Build.NetAssembly.Configuration=
+SET Arbor.Build.Tools.External.MSBuild.AllowPrerelease.Enabled=false
+SET Arbor.Build.Build.WebProjectsBuild.Enabled=false
+SET Arbor.Build.MSBuild.NuGetRestore.Enabled=true
+SET Arbor.Build.NuGet.ReinstallArborPackageEnabled=true
+SET Arbor.Build.NuGet.VersionUpdateEnabled=false
+SET Arbor.Build.Artifacts.PdbArtifacts.Enabled=true
+SET Arbor.Build.NuGet.Package.CreateNuGetWebPackages.Enabled=false
+SET Arbor.Build.Tools.External.Xunit.NetCoreApp.Enabled=false
+SET Arbor.Build.Tools.External.Xunit.NetFramework.Enabled=false
 
-SET Arbor.X.Build.NetAssembly.MetadataEnabled=true
-SET Arbor.X.Build.NetAssembly.Description=Key value configuration
-SET Arbor.X.Build.NetAssembly.Company=Niklas Lundberg
-SET Arbor.X.Build.NetAssembly.Copyright=© Niklas Lundberg 2014-2018
-SET Arbor.X.Build.NetAssembly.Trademark=Arbor.KVConfiguration
-SET Arbor.X.Build.NetAssembly.Product=Arbor.KVConfiguration
-SET Arbor.X.ShowAvailableVariablesEnabled=true
-SET Arbor.X.ShowDefinedVariablesEnabled=true
-SET Arbor.X.Tools.External.MSBuild.Verbosity=minimal
-SET Arbor.X.NuGet.Package.AllowManifestReWriteEnabled=false
+SET Arbor.Build.Build.NetAssembly.MetadataEnabled=true
+SET Arbor.Build.Build.NetAssembly.Description=Key value configuration
+SET Arbor.Build.Build.NetAssembly.Company=Niklas Lundberg
+SET Arbor.Build.Build.NetAssembly.Copyright=© Niklas Lundberg 2014-2018
+SET Arbor.Build.Build.NetAssembly.Trademark=Arbor.KVConfiguration
+SET Arbor.Build.Build.NetAssembly.Product=Arbor.KVConfiguration
+SET Arbor.Build.ShowAvailableVariablesEnabled=true
+SET Arbor.Build.ShowDefinedVariablesEnabled=true
+SET Arbor.Build.Tools.External.MSBuild.Verbosity=minimal
+SET Arbor.Build.NuGet.Package.AllowManifestReWriteEnabled=false
 
-SET Arbor.X.Tools.External.MSBuild.CodeAnalysis.Enabled=false
-SET Arbor.X.Build.PublishDotNetExecutableProjects=false
+SET Arbor.Build.Tools.External.MSBuild.CodeAnalysis.Enabled=false
+SET Arbor.Build.Build.PublishDotNetExecutableProjects=false
 
 CALL dotnet arbor-build
 
 REM Restore variables to default
 
-SET Arbor.X.Build.Bootstrapper.AllowPrerelease=
-REM SET Arbor.X.Vcs.Branch.Name=
-SET Arbor.X.Tools.External.MSpec.Enabled=
-SET Arbor.X.NuGet.Package.Artifacts.Suffix=
-SET Arbor.X.NuGet.Package.Artifacts.BuildNumber.Enabled=
-SET Arbor.X.Log.Level=
-SET Arbor.X.NuGetPackageVersion=
-SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled=
-SET Arbor.X.VariableOverrideEnabled=
-SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=
-SET Arbor.X.Build.NetAssembly.Configuration=
+SET Arbor.Build.Build.Bootstrapper.AllowPrerelease=
+REM SET Arbor.Build.Vcs.Branch.Name=
+SET Arbor.Build.Tools.External.MSpec.Enabled=
+SET Arbor.Build.NuGet.Package.Artifacts.Suffix=
+SET Arbor.Build.NuGet.Package.Artifacts.BuildNumber.Enabled=
+SET Arbor.Build.Log.Level=
+SET Arbor.Build.NuGetPackageVersion=
+SET Arbor.Build.Vcs.Branch.Name.Version.OverrideEnabled=
+SET Arbor.Build.VariableOverrideEnabled=
+SET Arbor.Build.Artifacts.CleanupBeforeBuildEnabled=
+SET Arbor.Build.Build.NetAssembly.Configuration=
 
 EXIT /B %ERRORLEVEL%
